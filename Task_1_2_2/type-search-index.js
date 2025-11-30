5ea20c251ba60a1899373900c5293dea70c7a1eb
Rakdat <Rakdat@users.noreply.github.com>
2025-11-30 12:38:26 +0000

    Deploying to gh-pages from @ Rakdat/OOP@48689080acdec2cc655936e4c15869e73021306a 🚀

---
+++ Task_1_2_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.tokarev4","l":"HashTable.Entry"},{"p":"ru.nsu.tokarev4","l":"HashTable"},{"p":"ru.nsu.tokarev4","l":"InteractiveConsole"}];updateSearchResults();
