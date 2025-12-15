0ebced23e2132ad2f5a7377198d3a3072e4a08a1
Rakdat <Rakdat@users.noreply.github.com>
2025-12-15 03:47:56 +0000

    Deploying to gh-pages from @ Rakdat/OOP@cca7e7ee3c22471fd2e06724cc95a1eb708a907e 🚀

--- Task_1_5_1/package-search-index.js
+++ Task_1_5_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.tokarev4"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.tokarev4"},{"l":"ru.nsu.tokarev4.markdown"},{"l":"ru.nsu.tokarev4.model"}];updateSearchResults();
